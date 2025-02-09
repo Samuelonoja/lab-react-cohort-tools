ed8e319b4eb7838ca6056000150995e978da5235
Samuel Onoja <samuelonoja0@gmail.com>
2025-02-09 01:03:16 -0800

    lab done

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
+import { Route, Router, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
 import StudentDetailsPage from "./pages/StudentDetailsPage";
 import UserProfilePage from "./pages/UserProfilePage";
+import studentsData from "./assets/students.json";
+import { useState } from "react";
 
 
 function App() {
 
+  const [students, setStudents] = useState(studentsData);
+
+
   return (
-    <div className="App relative z-20 pt-20">
+
+ <>
+  <div className="App relative z-20 pt-20">
       <Navbar />
 
-      <div className="pages">
+      {/* <div className="pages">
         <HomePage />
-
-        <StudentDetailsPage />
-
-        <UserProfilePage />
-      </div>
-
+      </div> */}
+      
+      
     </div>
+    <Routes>
+        <Route path="/" element = {<HomePage  />}/>
+        <Route path="/profile" element =  {<UserProfilePage />}/>
+        <Route path="/students/:studentId" element={<StudentDetailsPage/>}/>
+    </Routes>
+ </>
+ 
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </BrowserRouter>
 );
+
+
+  // Find the current student profile by id.
+  // In this case, the student with id 1. The `studentId` is hard-coded for now.
+  // This could be a URL parameter from React Router, e.g. /students/:studentId
+  // const studentId = "1";
+  // const studentProfile = studentsData.find((student) => student._id === studentId);
+
